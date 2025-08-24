5bfda1e03e7b06eaf4af7220d52bce800f7fa3c4
Sherin-Puthukkara <contact.me.sherin@gmail.com>
2025-08-24 23:37:04 +0530

    back to older version of css file tha has no toogle option

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 console.log("Sherin’s portfolio is alive ✨");
-const toggleBtn = document.getElementById("darkModeToggle");
-
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  toggleBtn.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
-});
