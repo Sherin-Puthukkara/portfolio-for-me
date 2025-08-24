4b474717cd14d82efd5003ff583519693f30aa2a
Sherin-Puthukkara <contact.me.sherin@gmail.com>
2025-08-24 22:43:14 +0530

    tooglemode creted

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
     msg.textContent = "Hello! Thanks for clicking 😊";
   });
 });
+const toggleBtn = document.getElementById("toggleMode");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
