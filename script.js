f3450197ce5341d44b367939f1e58517cb18f6c7
Sherin-Puthukkara <contact.me.sherin@gmail.com>
2025-08-24 22:19:36 +0530

    java script updated and added javascript interaction feature

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Sherin’s portfolio is alive ✨");
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("greetBtn");
+  const msg = document.getElementById("message");
+
+  btn.addEventListener("click", function () {
+    msg.textContent = "Hello! Thanks for clicking 😊";
+  });
+});
